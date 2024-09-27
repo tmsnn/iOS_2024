cbcccd8f7f4887fb733be4240b2a33a717a75b37
Tomiris <98753027+tmsnn@users.noreply.github.com>
2024-09-27 10:52:46 +0500

    Add files via upload

---
+++ assignment3/I-Am-Rich/I-Am-Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I-Am-Rich
+//
+//  Created by Aruzhan Kaharmanova on 26.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
